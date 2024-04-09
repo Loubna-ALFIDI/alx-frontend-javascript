0292cf3f950fb17abd958cb0961cc51b992255ad
Loubna <loubna.alfidi@gmail.com>
2024-04-09 02:58:31 +0000

    Let us create a report object

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+}
