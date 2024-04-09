bcd1c0a5532197bba5cf21a3817b143cf2b5243b
Loubna <loubna.alfidi@gmail.com>
2024-04-09 02:55:22 +0000

    For...of Loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
+  }
+  return newArray;
+}
