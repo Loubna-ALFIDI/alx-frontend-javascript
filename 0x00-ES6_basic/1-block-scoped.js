cdc331928b9bb192adf3bee83c150b164047acd6
Loubna <loubna.alfidi@gmail.com>
2024-04-09 00:13:58 +0000

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 export default function taskBlock(trueOrFalse) {
-    var task = false;
-    var task2 = true;
-  
-    if (trueOrFalse) {
-      let task = true;
-      let task2 = false;
-    }
-  
-    return [task, task2];
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
   }
+
+  return [task, task2];
+}
