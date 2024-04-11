6bb7c3ddb93cb4c5d42acc954fcf07178dc5a0d6
Loubna <loubna.alfidi@gmail.com>
2024-04-11 02:45:25 +0000

    Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+export default function handleResponseFromAPI(promise) {
+ return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
+}
