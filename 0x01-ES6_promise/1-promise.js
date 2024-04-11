864ac3d8011036da800a21c3ec74c94ea048aa70
Loubna <loubna.alfidi@gmail.com>
2024-04-11 02:46:16 +0000

    fix style

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 export default function getFullResponseFromAPI(success) {
- if (success) return Promise.resolve({ status: 200, body: 'Success' });
- return Promise.reject(new Error('The fake API is not working currently'));
+  if (success) return Promise.resolve({ status: 200, body: 'Success' });
+  return Promise.reject(new Error('The fake API is not working currently'));
 }
