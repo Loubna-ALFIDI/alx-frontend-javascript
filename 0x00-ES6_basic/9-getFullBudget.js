8e8340073d8a9083ee63eaca71c2b7d30d82f984
Loubna <loubna.alfidi@gmail.com>
2024-04-11 03:10:22 +0000

    ES6 method properties

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,6 +1,4 @@
-#!/usr/bin/node
 import getBudgetObject from './7-getBudgetObject';
-
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
