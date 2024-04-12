8f27839ff337feb4f365f2ec9c086575974dfa68
Loubna <loubna.alfidi@gmail.com>
2024-04-12 02:37:02 +0000

    Pricing

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,18 +1,17 @@
 export default class Building {
-    constructor(sqft) {
-      if (new.target !== Building
+  constructor(sqft) {
+    if (new.target !== Building
           && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
-      }
-      this._sqft = sqft;
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-  
-    get sqft() {
-      return this._sqft;
-    }
-  
-    set sqft(sqft) {
-      this._sqft = sqft;
-    }
+    this._sqft = sqft;
   }
-  
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    this._sqft = sqft;
+  }
+}
