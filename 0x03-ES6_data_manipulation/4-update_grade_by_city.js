e6a96b51de2b70a292039c55b7d200a21a62b2e9
Loubna <loubna.alfidi@gmail.com>
2024-04-15 21:20:58 +0100

    Combine

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,15 @@
-#!/usr/bin/node
+function updateStudentGradeByCity(studentsArray, city, newGrades) {
+  if (!Array.isArray(studentsArray)) {
+    return [];
+  }
+
+  return studentsArray
+  .filter(student => student.location === city)
+  .map(student => {
+      const gradeObj = newGrades.find(grade => grade.studentId === student.id);
+      const grade = gradeObj ? gradeObj.grade : 'N/A';
+      return { ...student, grade };
+  });
+}
+
+export default updateStudentGradeByCity;
