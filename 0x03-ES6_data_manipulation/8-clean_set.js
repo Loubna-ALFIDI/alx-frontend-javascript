cbc1bfaa378d1b951ef4e27c7aacddc06ffac5c8
Loubna <loubna.alfidi@gmail.com>
2024-04-15 23:30:52 +0100

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 function cleanSet(set, startString) {
   if (!(set instanceof Set) || typeof startString !== 'string') {
     return '';
-}
+  }
 
   return Array.from(set)
-  .filter(value => value.startsWith(startString))
-  .map(value => value.replace(startString, ''))
-  .join('-');
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.replace(startString, ''))
+    .join('-');
 }
 
 export default cleanSet;
