4fce326260f73ad285ce339f886e0494e21d9a8d
Loubna <loubna.alfidi@gmail.com>
2024-04-15 21:46:14 +0100

    Typed Arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  const array = new ArrayBuffer(length);
+  const view = new Int8Array(array);
+  view[position] = value;
+
+  return array;
+}
+
+export default createInt8TypedArray;
