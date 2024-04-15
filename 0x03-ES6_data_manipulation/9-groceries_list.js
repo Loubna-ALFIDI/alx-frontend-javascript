5a3178d60c2cf67fd202404d9032d85d6de44a14
Loubna <loubna.alfidi@gmail.com>
2024-04-15 23:43:00 +0100

    Map data structure

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+function groceriesList() {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
+
+export default groceriesList;
