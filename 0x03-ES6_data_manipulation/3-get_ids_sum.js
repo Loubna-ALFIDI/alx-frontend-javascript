1081bf43b0ad4196f42295e628f63497febfa5a7
Loubna <loubna.alfidi@gmail.com>
2024-04-15 21:08:36 +0100

    Reduce

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+function getStudentIdsSum(studentsArray) {
+  if (!Array.isArray(studentsArray)) {
+    return [];
+  }
+
+  return studentsArray.reduce((sum, student) => sum + student.id, 0);
+}
+
+export default getStudentIdsSum;
